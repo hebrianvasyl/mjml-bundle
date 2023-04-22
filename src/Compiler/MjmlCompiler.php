b17c9241a96920187514797540cae4cd3ae926e2
hebrian <hebrian.vasyl@gmail.com>
2023-04-22 10:50:40 +0300

    use mjml bin from project node_modules

--- src/Compiler/MjmlCompiler.php
+++ src/Compiler/MjmlCompiler.php
@@ -11,10 +11,23 @@ use Symfony\Component\Process\Process;
  */
 class MjmlCompiler implements MjmlCompilerInterface
 {
+    protected $projectDir;
+
+    public function __construct(
+        string $projectDir
+    ) {
+        $this->projectDir = $projectDir;
+    }
+
     public function compile(string $input, string $output)
     {
+        $path = $this->projectDir . '/node_modules/.bin/mjml';
+        if (!file_exists($path)) {
+            throw new \Exception('MJML is not installed. Please run "npm install mjml" in your project directory.');
+        }
+
         $command = [
-            'mjml',
+            $path,
             $input,
             '-o',
             $output,
